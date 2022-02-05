fe848938a4132867cbe78709268d7a889b1d7634
dhino12 <dhinorahmad0@gmail.com>
2022-02-05 21:09:55 +0700

    add header component

---
+++ pages/components/header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <h1 className="logo">DR</h1>
+            <h2 className="sayHello">Say Hi....</h2>
+        </header>
+    )
+}
