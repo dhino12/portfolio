64ef3a5e2b92c8393cd6d3f5c088c912b7d61b7a
dhino12 <dhinorahmad0@gmail.com>
2022-02-12 17:04:59 +0700

    add logo

--- pages/components/header.js
+++ pages/components/header.js
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header>
-            <h1 className="logo">DR</h1>
+            <h1 className="logo">.D.</h1>
             <h2 className="sayHello">Say Hi....</h2>
 
             <button type="submit"> Light </button>
