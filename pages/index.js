26f77cb4d3511c910aabaa34ddb7fb4f5bd3e8a4
dhino12 <dhinorahmad0@gmail.com>
2022-02-23 08:45:04 +0700

    fix next/image not support for static pages

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// import Image from "next/image"; not support static next
 import Lottie from "lottie-react";
 import Link from "next/link";
 import { motion } from "framer-motion";
