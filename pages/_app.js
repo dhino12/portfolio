7b56a69935d2d692587e741ed90eaaff6d94b02a
dhino12 <dhinorahmad0@gmail.com>
2022-03-03 19:46:53 +0700

    add animatePrecence

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import { AnimatePresence } from 'framer-motion'
 import '../styles/globals.css'
 import '../styles/home.scss'
+import '../styles/certificate.scss'
+import '../styles/background.css'
 import '../styles/mediaQuery/homeResponsive.scss'
+import '../styles/mediaQuery/certifResponsive.scss'
+import '../styles/mediaQuery/repoResponsive.scss'
+import '../styles/mediaQuery/aboutResponsive.scss'
 
 function MyApp({ Component, pageProps, router }) {
   return (
