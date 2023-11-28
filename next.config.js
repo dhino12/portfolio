04e9e30a8892b163f16d8268372ecfd25a359553
dhino12 <dhinorahmad0@gmail.com>
2023-11-28 20:21:42 +0700

    nextJS Supabase

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: true, 
+  images: {
+    domains: [
+        'gjamaowmsyukioirshbv.supabase.co',
+        "localhost"
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.example.com',
+        port: '',
+        pathname: '/account123/**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 }
